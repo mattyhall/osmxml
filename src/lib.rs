862fe35d05cb715138d1fdefdb5ab171ff318796
Matthew Hall <matthew@quickbeam.me.uk>
2014-08-16 18:12:11 +0100

    Handle tags
    Tags can be on ways and nodes. We no longer keep track of the depth and
    instead expect only tags to be children of nodes and ways and then check
    for the closing of tags and nodes and ways

--- src/lib.rs
+++ src/lib.rs
@@ -4,20 +4,20 @@ extern crate sax;
 
 use std::collections::HashMap;
 
-type Tags<'a> = HashMap<&'a str, &'a str>;
+type Tags = HashMap<String, String>;
 
 #[deriving(Show)]
-pub enum OsmElement<'a> {
-    Node{id: int, lat: f64, lng: f64, visible: bool, tags: Tags<'a>},
-    Way{id: int, nodes: Vec<int>},
+pub enum OsmElement {
+    Node{id: int, lat: f64, lng: f64, visible: bool, tags: Tags},
+    Way{id: int, nodes: Vec<int>, tags: Tags},
 }
 
-pub struct Osm<'a> {
+pub struct Osm {
     pub parser: Receiver<sax::ParseResult>,
-    pub elements: HashMap<int, OsmElement<'a>>,
+    pub elements: HashMap<int, OsmElement>,
 }
 
-impl<'a> Osm<'a> {
+impl Osm {
     pub fn new(path: &Path) -> Osm {
         let parser = sax::parse_file(path).unwrap();
         Osm {parser: parser, elements: HashMap::new()}
@@ -52,53 +52,87 @@ impl<'a> Osm<'a> {
         let lat = from_str(attrs.get("lat")).unwrap();
         let lng = from_str(attrs.get("lon")).unwrap();
         let visible = from_str(attrs.get("visible")).unwrap();
-        self.elements.insert(id, Node{id: id, lat: lat, lng: lng, visible: visible, tags: HashMap::new()});
+        let mut tags = HashMap::new();
 
-        let mut depth: int = 1;
         for event in self.parser.iter() {
             match event {
-                Ok(sax::StartElement(..)) => depth += 1,
-                Ok(sax::EndElement(..)) => {
-                    depth -= 1;
-                    if depth <= 0 {
-                        return;
+                Ok(sax::StartElement(name, attrs)) => {
+                    match name.as_slice() {
+                        "tag" => self.parse_tag(attrs, &mut tags),
+                        _ => fail!("Wrong thing"),
                     }
+                }
+                Ok(sax::EndElement(name)) => {
+                    if name.as_slice() == "node" {
+                        break;
+                    }
+                    fail!("Wrong thing");
                 }
                 _ => (),
             }
         }
+
+        self.elements.insert(id, Node{id: id, lat: lat, lng: lng, visible: visible, tags: tags});
     }
 
     fn parse_way(&mut self, attrs: sax::Attributes) {
         let id = from_str(attrs.get("id")).unwrap();
         let mut nodes = Vec::new();
-        let mut depth: int = 1;
+        let mut tags = HashMap::new();
+
         for event in self.parser.iter() {
             match event {
                 Ok(sax::StartElement(name, attrs)) => {
-                    depth += 1;
                     match name.as_slice() {
                         "nd" => nodes.push(self.parse_nd(attrs)),
-                        _ => (),
+                        "tag" => self.parse_tag(attrs, &mut tags),
+                        _ => fail!("Wrong thing {} {}", name, attrs),
                     }
                 }
-                Ok(sax::EndElement(..)) => {
-                    depth -= 1;
-                    if depth <= 0 {
+                Ok(sax::EndElement(name)) => {
+                    if name.as_slice() == "way" {
                         break;
                     }
+                    fail!("Wrong thing");
                 }
                 _ => (),
             }
         }
-        self.elements.insert(id, Way {id: id, nodes: nodes});
+        self.elements.insert(id, Way {id: id, nodes: nodes, tags: tags});
     }
 
     fn parse_nd(&mut self, attrs: sax::Attributes) -> int {
-        from_str(attrs.get("ref")).unwrap()
+        let i = from_str(attrs.get("ref")).unwrap();
+
+        for event in self.parser.iter() {
+            match event {
+                Ok(sax::EndElement(name)) => {
+                    if name.as_slice() == "nd" {
+                        break;
+                    }
+                    fail!("Wrong thing");
+                }
+                _ => fail!("Wrong thing")
+            }
+        }
+        return i;
+    }
+
+    fn parse_tag(&mut self, attrs: sax::Attributes, tags: &mut Tags) {
+        let (k, v) = (attrs.get_clone("k"), attrs.get_clone("v"));
+        for event in self.parser.iter() {
+            match event {
+                Ok(sax::EndElement(name)) => {
+                    if name.as_slice() == "tag" {
+                        tags.insert(k, v);
+                        return;
+                    }
+                }
+                _ => fail!("Got wrong thing"),
+            }
+        }
     }
 
     fn parse_relation(&mut self) {
-        println!("Got relation");
     }
 }
